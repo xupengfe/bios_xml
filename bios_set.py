b7a58d35519e31fb14b66584f287466861e5f6cc
Pengfei Xu <pengfei.xu@intel.com>
2019-06-20 23:48:53 +0000

    bios_set.py: add return error when set xml enable failed
    1. add return error when set xml enable failed
    2. add ini path when set bios successfully
    
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- bios_set.py
+++ bios_set.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python2.7
 
-
 import os, sys, getopt
-XML_PATH = '/root/bios_xml/'
-XML_FILE = '/root/target.xml'
+XML_PATH = os.path.dirname(os.path.realpath(__file__))
+#XML_PATH = '/root/bios_xml/'
+XML_FILE = XML_PATH + '/target.xml'
+INI_PATH = XML_PATH + '/out/TmpBiosKnobs.ini'
 
 def set_default_bios():
     sys.path.append(r"%s"%(XML_PATH))
     import XmlCli as cli
     cli.clb._setCliAccess("linux")
-    cli.clb.ConfXmlCli()
+    try:
+        cli.clb.ConfXmlCli()
+    except:
+        print 'error'
+        sys.exit(2)
     cli.CvLoadDefaults()
 
 def get_xml():
     sys.path.append(r"%s"%(XML_PATH))
     import XmlCli as cli
     cli.clb._setCliAccess("linux")
-    cli.clb.ConfXmlCli()
+    try:
+        cli.clb.ConfXmlCli()
+    except:
+        print 'error'
+        sys.exit(2)
     cli.savexml(r"%s"%(XML_FILE))
     print XML_FILE
 
@@ -24,7 +33,11 @@ def set_bios(bios_items):
     sys.path.append(r"%s"%(XML_PATH))
     import XmlCli as cli
     cli.clb._setCliAccess("linux")
-    cli.clb.ConfXmlCli()
+    try:
+        cli.clb.ConfXmlCli()
+    except:
+        print 'error'
+        sys.exit(2)
     cli.CvProgKnobs("%s"%(bios_items))
     print 'Set ' + bios_items + ' done.'
 
@@ -43,6 +56,7 @@ def main(argv):
             print sys.argv[0] + ' -g "default|current" | -s "SecurityMode=0x00, TBTHotSMI=0x00" | -d "SecurityMode=0x00, TBTHotSMI=0x00"'
             print 'For example:'
             print 'python2.7 bios_set.py -d "DiscreteTbtSupport=0x1,TbtBootOn=0x2,TBTHotSMI=0x0,Gpio5Filter=0x0,TBTHotNotify=0x0,DTbtController_0=0x1,TBTSetClkReq=0x1,TbtLtr=0x1,DTbthostRouterPortNumber_0=0x2,DTbtPcieExtraBusRsvd_0=0x6A,DTbtPcieMemRsvd_0=0x6A,DTbtPcieMemRsvd_0=0x2E1,DTbtPciePMemRsvd_0=0x4A0,Win10Support=0x2,TrOsup=0x1,TbtL1SubStates=0x0,Rtd3Tbt=0x1,TbtVtdBaseSecurity=0x1,EnableSgx=0x1,PrmrrSize=0x8000000,EnableAbove4GBMmio=0x1,PrimaryDisplay_inst_3=0x0,PcieRootPortAspm_20=0x2,PcieRootPortHPE_20=0x1,PcieRootPortDptp_20=0x5,PcieSwEqOverride=0x1"'
+            print 'bios_xml folder path: ' + XML_PATH
             sys.exit()
 
         elif opt in ("-g", "--gId"):
@@ -60,12 +74,14 @@ def main(argv):
             print 'Set bios items:' + bios_set_items
             set_bios(bios_set_items)
             get_xml()
+            print INI_PATH
         elif opt in ("-d", "--dId"):
             bios_set_items = arg
             print 'Set bios items based on default BIOS setting:' + bios_set_items
             set_default_bios()
             set_bios(bios_set_items)
             get_xml()
+            print INI_PATH
 
 if __name__ == "__main__":
     main(sys.argv[1:])
